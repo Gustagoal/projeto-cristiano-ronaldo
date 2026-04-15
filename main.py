2a97d03d3ae4ffe8c27e003e905dc3b310161811
Gustagoal <gustavocastruu@gmail.com>
2026-04-15 00:05:05 -0300

    Adicionando um layout

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 import pandas as pd 
 import streamlit as st 
-import openpyxl , plotly
+import openpyxl , plotly , lxml
 
-# url = "https://www.espn.com.br/futebol/jogador/estatisticas/_/id/22774/cristiano-ronaldo"
+img = "/home/gustavo/Área de trabalho/cristiano-ronaldo/Cristiano-Ronaldo-No-Background.png"
 
-table = pd.read_excel("dados.xlsx")
+# url = r"https://www.365scores.com/pt-br/news/magazine/quantos-gols-cristiano-ronaldo-tem-na-carreira/"
 
+#table = pd.read_html(url)
+#tabela = pd.DataFrame(table[0],index=None)
 
-st.title("Estaticas por ano")
-st.bar_chart(table,x="Ano" ,y=["Gols",'Assistencias',"Titular"] , width=300 , height=400)
+#tabela.to_excel("dados.xlsx")
 
+import pandas as pd
+import streamlit as st
 
+df = pd.read_excel("dados.xlsx")
+
+import pandas as pd
+import streamlit as st
+
+df = pd.read_excel("dados.xlsx")
+
+with st.sidebar:
+    clube = st.selectbox("Clube", df["Clube"].unique())
+    dados = df[df["Clube"] == clube]
+    st.metric("Gols", dados["Gols"].sum())
+    st.metric("Jogos", dados["Jogos"].sum())
+    st.metric("Média", dados["Gols"].mean())
+    
+st.title("Meta : 1.000 Gols")
+
+st.metric("Gols",df["Gols"].sum())
+st.metric("Jogos", df["Jogos"].sum())
+
+    
+st.image(img)
