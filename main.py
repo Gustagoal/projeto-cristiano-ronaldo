f0545971bdcc23cc4f0851a32f06a51c7410d7aa
Gustagoal <gustavocastruu@gmail.com>
2026-04-15 22:39:38 -0300

    Adicionando icones

--- main.py
+++ main.py
@@ -22,15 +22,15 @@ df = pd.read_excel("dados.xlsx")
 with st.sidebar:
     clube = st.selectbox("Clube", df["Clube"].unique())
     dados = df[df["Clube"] == clube]
-    st.metric("Gols", dados["Gols"].sum())
-    st.metric("Jogos", dados["Jogos"].sum())
+    st.metric("Gols ⚽", dados["Gols"].sum())
+    st.metric("Jogos 🥅 ", dados["Jogos"].sum())
     media = dados["Gols"].sum() / dados["Jogos"].sum()
-    st.metric("Média",f"{media:.2f}")
+    st.metric("Média 📊",f"{media:.2f}")
     
-st.title("Meta : 1.000 Gols")
+st.title("Meta 🥇: 1.000 Gols")
 
-st.metric("Gols",df["Gols"].sum())
-st.metric("Jogos", df["Jogos"].sum())
+st.metric("Gols ⚽️",df["Gols"].sum())
+st.metric("Jogos 🥅", df["Jogos"].sum())
 
 img = r"img/Cristiano-Ronaldo-No-Background.png"    
 st.image(img)
