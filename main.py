98b21fd16f6835a788cb57a73abed10d35656cb8
Gustagoal <gustavocastruu@gmail.com>
2026-04-17 00:47:29 -0300

    Subindo o projeto com ajuda da IA para o teste

--- main.py
+++ main.py
@@ -1,49 +1,45 @@
-import pandas as pd 
-import streamlit as st 
+import pandas as pd
+import streamlit as st
 import openpyxl , plotly , lxml
 
-# url = r"https://www.365scores.com/pt-br/news/magazine/quantos-gols-cristiano-ronaldo-tem-na-carreira/"
-
-#url_calendario = "https://www.espn.com.br/futebol/time/calendario/_/id/817"
-
-url_portugal = "https://www.espn.com.br/futebol/time/calendario/_/id/482/por"
+#url_portugal = "https://www.espn.com.br/futebol/time/calendario/_/id/482/por"
 
 
-table = pd.read_html(url_portugal)
-tabela = pd.DataFrame(pd.concat([table[0],table[1],table[2],table[3]]),index=None)
 
-tabela.to_excel("calendario_portugal.xlsx")
+#table = pd.read_html(url_portugal)
+#tabela = pd.concat(table, ignore_index=True)
+
+#tabela.to_excel("calendario_portugal.xlsx")
 
 calendario = pd.read_excel("calendario.xlsx")
 calendario_portugal = pd.read_excel("calendario_portugal.xlsx")
-
 
 df = pd.read_excel("dados.xlsx")
 
 with st.sidebar:
     clube = st.selectbox("Clube", df["Clube"].unique())
     dados = df[df["Clube"] == clube]
-    st.metric("Gols ⚽", dados["Gols"].sum())
-    st.metric("Jogos 🥅 ", dados["Jogos"].sum())
-    media = dados["Gols"].sum() / dados["Jogos"].sum()
+    gols = dados["Gols"].sum()
+    jogos = dados["Jogos"].sum()
+    media = gols / jogos if jogos != 0 else 0
+    st.metric("Gols ⚽", gols)
+    st.metric("Jogos 🥅 ", jogos)
     st.metric("Média 📊",f"{media:.2f}")
-    
-st.title("Meta 🥇: 1.000 Gols")
 
+st.title("Meta 🥇: 1.000 Gols")
 st.metric("Gols ⚽️",df["Gols"].sum())
 st.metric("Jogos 🥅", df["Jogos"].sum())
+img = r"img/Cristiano-Ronaldo-No-Background.png"
+st.image(img,width=400)
 
 opcao = st.selectbox(
-    "Calendário",
-    ["Al-Nassr", "Portugal"],
-    index=None,
-    placeholder="Selecione um time"
+"Calendário",
+["Al-Nassr", "Portugal"],
+index=None,
+placeholder="Selecione um time"
 )
 
 if opcao == "Al-Nassr":
-    st.table(calendario)
+    st.dataframe(calendario,hide_index=True)
 elif opcao == "Portugal":
-    st.table(calendario_portugal)
-    
-img = r"img/Cristiano-Ronaldo-No-Background.png"    
-st.image(img)
+    st.dataframe(calendario_portugal,hide_index=True)
